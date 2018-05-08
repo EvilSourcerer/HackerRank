6cbdd04929bf6fb443cde23af49bb3f0a9d5eed6
EvilSourcerer <32974335+EvilSourcerer@users.noreply.github.com>
2018-05-08 16:04:15 -0400

    Removed unnecessary packages, added package.

--- HackerRank/Algorithms/implementation/The_Time_in_Words.java
+++ HackerRank/Algorithms/implementation/The_Time_in_Words.java
@@ -1,8 +1,4 @@
-package implementation;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.Scanner;
 /**
  * @author Aakansha Doshi
  * 
